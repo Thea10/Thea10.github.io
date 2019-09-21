8d5725d208602c990f0b95d01375e9c39f826928
Thea Abigail <olucynakin@gmail.com>
2019-09-21 14:50:07 +0100

    styling and images, basic JS files

---
+++ js/type.js
@@ -0,0 +1,35 @@
+//import 'typed.js';
+
+var options = {
+  strings: ["Hello, ^500 I'm Cynthia", "I'm into -- ^1000Front-End Development", "HTML5, ^1000 CSS3,^1000 Bootstrap 4, ^1000 Vanilla JS,^1000 React,^1000 Redux", "Hello, ^2000 I'm Cynthia"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  showCursor: false,
+}
+
+const type = document.getElementById('type');
+
+var typed = new Typed(type, options);
+
+
+
+window.addEventListener('scroll', (e) => {
+  var nav = document.getElementById('nav');
+  //  var navbtn = nav.querySelectorAll('button');
+  //to change the background on scroll
+  if (window.scrollY > 250) {
+    nav.classList.add('nav-scrolled')
+  } else if (nav.classList.contains('nav-scrolled')) {
+    nav.classList.remove("nav-scrolled");
+  
+  }
+
+  /* navbtn.forEach(btn => {
+     if (nav.classList.contains('header-scrolled')) {
+       btn.classList.add('btn-scrolled')
+     } else {
+       btn.classList.remove('btn-scrolled')
+     }
+   }); */
+
+})
